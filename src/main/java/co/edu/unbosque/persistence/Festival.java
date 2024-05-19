884c5f8afd398ebe9100c00c10b3d2613d05ede8
JuanOyolaGalindo <114842542+JuanOyolaGalindo@users.noreply.github.com>
2024-05-19 11:47:50 -0500

    Update Festival.java

--- src/main/java/co/edu/unbosque/persistence/Festival.java
+++ src/main/java/co/edu/unbosque/persistence/Festival.java
@@ -1,33 +1,58 @@
 package co.edu.unbosque.persistence;
 
-
 import java.time.LocalDate;
 
-
+/**
+ * La clase Festival representa un festival con su nombre y fecha de fundación.
+ */
 public class Festival {
     private String festival;
     private LocalDate fundacion;
 
+    /**
+     * Constructor de la clase Festival.
+     * 
+     * @param festival  El nombre del festival.
+     * @param fundacion La fecha de fundación del festival.
+     */
     public Festival(String festival, LocalDate fundacion) {
         this.festival = festival;
         this.fundacion = fundacion;
     }
 
+    /**
+     * Obtiene el nombre del festival.
+     * 
+     * @return El nombre del festival.
+     */
     public String getFestival() {
         return festival;
     }
 
+    /**
+     * Establece el nombre del festival.
+     * 
+     * @param festival El nombre del festival.
+     */
     public void setFestival(String festival) {
         this.festival = festival;
     }
 
+    /**
+     * Obtiene la fecha de fundación del festival.
+     * 
+     * @return La fecha de fundación del festival.
+     */
     public LocalDate getFundacion() {
         return fundacion;
     }
 
+    /**
+     * Establece la fecha de fundación del festival.
+     * 
+     * @param fundacion La fecha de fundación del festival.
+     */
     public void setFundacion(LocalDate fundacion) {
         this.fundacion = fundacion;
     }
-
-
 }
